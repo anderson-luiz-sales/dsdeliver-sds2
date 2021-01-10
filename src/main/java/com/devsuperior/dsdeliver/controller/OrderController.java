264d170ffb2585382020b4d7d2d292ac2700919e
andlsales95 <anderson.sales@technosoftware.com.br>
2021-01-10 10:04:35 -0300

    GET /orders

---
+++ src/main/java/com/devsuperior/dsdeliver/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsdeliver.controller;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.services.OrderService;
+import com.devsuperior.dsdeliver.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+@CrossOrigin(origins = "*")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findAll() {
+        return new ResponseEntity<>(orderService.findAll(), HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
     List<Product> findAllByOrderByNameAsc();
 }
